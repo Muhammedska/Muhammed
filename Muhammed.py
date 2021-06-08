49d4622c75ac2c91a90cb65d2000e4c9808deabb
SafetyZone <81029510+Muhammedska@users.noreply.github.com>
2021-06-08 18:00:43 +0300

    Update Muhammed.py

--- Muhammed.py
+++ Muhammed.py
@@ -8,8 +8,25 @@ char_3 = '!^+%&/()=?_-,;:.#${[]}\\|@'
 
 class Muhammed:
     def Muhammed_iptal(**param):
-        print('işlem iptal edildi')
-    
+        return 'işlem iptal edildi'
+
+    def Muhammed_bosluk(**param):
+        return ('\n' +' '*8 +'-' * 40 + '\n')
+
+    def Muhammed_request(**param):
+        var_muhammed = """
+        ++++++++++++++++++++++++++++++
+        | num | Türler               |
+        |+++++|++++++++++++++++++++++|
+        |  1  | sadece küçük harf    |
+        |  2  | büyük  küçük harf    |
+        |  3  | rakam ve harf        |
+        |  4  | rakam harf noktalama |
+        |+++++|++++++++++++++++++++++|
+        ++++++++++++++++++++++++++++++
+        """
+        return var_muhammed
+
     def Muhammed_1(**param):
         var_muhammed = """
         ###### ######  |  #    # | #
@@ -38,4 +55,20 @@ class Muhammed:
 
 
 if __name__ == "__main__":
+    system_run = True
     print(Muhammed.Muhammed_1())
+    print(Muhammed.Muhammed_bosluk())
+    print(Muhammed.Muhammed_request())
+    while system_run:
+        pusat = input('Şifre Güvenlik Düzeyi Seçiniz :\n> ')
+        if str(pusat) == '1':
+            print('1 Numaralı işlem seçildi')
+        elif str(pusat) == '2':
+            print('2 Numaralı işlem seçildi')
+        elif str(pusat) == '3':
+            print('3 Numaralı işlem seçildi')
+        elif str(pusat) == '4':
+            print('4 Numaralı işlem seçildi')
+        else:
+            print(' '*8+'Tanımlanayan komut > komutlar')
+            print(Muhammed.Muhammed_request())
